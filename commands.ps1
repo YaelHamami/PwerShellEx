25091438bf4a73874555a98546c6728a32865e9b
unknown <yael.hamami@gmail.com>
2021-07-12 10:39:24 +0000

    add 2 more exercises

--- commands.ps1
+++ commands.ps1
@@ -1,9 +1,22 @@
 # Write yael in Cyan
 Write-Host -ForegroundColor Cyan yael
 
-
-# # convert bytes to GB 
-
+# Convert bytes to GB 
 Write-Host enter number of bytes to convert to GB
 $bytes = Read-Host
+
 Write-Host $bytes bytes is ($bytes/1000) GB
+
+# 2 input strings into 1
+Write-Host Please enter a string
+$firstString = Read-Host
+
+Write-Host Please enter another string
+$secString = Read-Host
+
+Write-Host $firstString$secString
+
+# count items in an array
+$array = @(3,4,5,6,8,3)
+Write-Host $array.Count
+
