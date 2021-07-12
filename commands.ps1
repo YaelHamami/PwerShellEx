97821cd248b3d225e3887824d2ef902f3d2e8adf
unknown <yael.hamami@gmail.com>
2021-07-12 11:06:19 +0000

    add 2 more exercises (number 5 and 6)

--- commands.ps1
+++ commands.ps1
@@ -20,3 +20,18 @@ Write-Host $firstString$secString
 $array = @(3,4,5,6,8,3)
 Write-Host $array.Count
 
+# Write values and keys of the hash table
+$hash = @{ Number = 1; Shape = "Square"; Color = "Blue"}
+write-host The values of the hash: $hash.Values
+write-host The keys of the hash: $hash.Keys
+
+#if the input is my name it will give YSE for an output, else NO
+$myName = "yael"
+Write-Host Enter a name 
+$inputName = Read-Host 
+
+if ($inputName -eq $myName){
+    Write-Host YES
+} else {
+    Write-Host NO
+}
